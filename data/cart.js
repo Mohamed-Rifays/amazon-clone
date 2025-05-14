e80112bce6438dd13b4563ba87515b9fd0384cef
Mohamed Rifays <rifaysmohamed2005@gmail.com>
2025-05-14 22:29:29 +0530

    make delivery options interactive

--- data/cart.js
+++ data/cart.js
@@ -68,3 +68,18 @@ export function addToCart(productId){
         matchingItem.quantity=newQuantity;
         saveToStorage();
       }
+
+      export function updateDeliveryOption(productId,deliveryOptionId){
+
+         let matchingItem;
+            
+              cart.forEach((cartItem)=>{
+              if(productId===cartItem.productId){
+                matchingItem=cartItem;
+              }
+              })
+             matchingItem.deliveryOptionId
+             =deliveryOptionId;
+
+             saveToStorage();
+      }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart,removeFromCart,updateQuantity} from '../data/cart.js';
+import {cart,removeFromCart,updateQuantity,updateDeliveryOption} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/monney.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
@@ -94,7 +94,9 @@ function deliveryOptionsHTML(matchingProduct,cartItem){
   const isChecked=deliveryOption.id===cartItem.deliveryOptionId;
 
  deliveryOptionHTML+=  `
-      <div class="delivery-option">
+      <div class="delivery-option js-delivery-option"
+      data-product-id="${matchingProduct.id}"
+      data-delivery-option-id="${deliveryOption.id}">
                   <input type="radio"
                   ${isChecked ? 'checked' : ''}
                     class="delivery-option-input"
@@ -182,6 +184,13 @@ document.querySelectorAll('.save-quantity-link').forEach((link)=>{
  })
 })
 
+document.querySelectorAll('.js-delivery-option').forEach((element)=>{
+  element.addEventListener('click',()=>{
+    const {productId,deliveryOptionId}=element.dataset;
+   updateDeliveryOption(productId,deliveryOptionId)
+  })
+
+})
 
   
 
